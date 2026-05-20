4a45826f64d7d91af693f4ffc676b4c8d72819eb
yuanzhao-CVLAB <36430674+yuanzhao-CVLAB@users.noreply.github.com>
2026-05-20 20:27:00 +0800

    Update eval_reasonseg.sh

--- run_scripts/eval_reasonseg.sh
+++ run_scripts/eval_reasonseg.sh
@@ -4,7 +4,7 @@ model=/paht/to/model
 
 ###### evalution reasonseg val ########
 echo "----------------------evalution Reasoning Segmentation  Val start ----------------------"
-CUDA_VISIBLE_DEVICES=3 python3.10 -m torch.distributed.run   --nproc_per_node 1 --standalone  \
+CUDA_VISIBLE_DEVICES=3 python -m torch.distributed.run   --nproc_per_node 1 --standalone  \
 --master_port="25999" src/eval/evaluate_reasonseg.py \
 --model_path  $model --cot
 
@@ -13,7 +13,7 @@ echo "----------------------evalution Reasoning Segmentation  Val start --------
 
 echo "----------------------evalution Reasoning Segmentation  Test start ----------------------"
 ###### evalution reasonseg test ########
-CUDA_VISIBLE_DEVICES=3 python3.10 -m torch.distributed.run   --nproc_per_node 1 --standalone  \
+CUDA_VISIBLE_DEVICES=3 python -m torch.distributed.run   --nproc_per_node 1 --standalone  \
 --master_port="25999" src/eval/evaluate_reasonseg.py \
 --model_path  $model
 echo "----------------------evalution Reasoning Segmentation  Test start ----------------------"
